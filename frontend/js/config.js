febd86082d9a49e364cb1d2bd5544bc5abea43fa
Varsha Jairam <Varsha.98948@gmail.com>
2026-07-19 15:54:51 +0530

    Multi commit issue resolved

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,9 @@
-window.GG_CONFIG = window.GG_CONFIG || {
-  apiBaseUrl: 'https://greengrid-byh0.onrender.com',
+const GG_DEFAULT_API_BASE_URL =
+  window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://localhost:8080'
+    : 'https://greengrid-byh0.onrender.com';
+
+window.GG_CONFIG = {
+  apiBaseUrl: GG_DEFAULT_API_BASE_URL,
+  ...window.GG_CONFIG,
 };
